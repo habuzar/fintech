e8ed5eaa770846a39af337a891adc701eb5b8323
habuzar <habuzar@uwo.ca>
2024-08-13 00:09:05 -0400

    Latest version working api

--- HomePage/news.js
+++ HomePage/news.js
@@ -1,59 +1,49 @@
 
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '8a639c38c3msh285c7676bda8bcdp1bd5fcjsne6a7fc32ba82',
-		'x-rapidapi-host': 'cnbc.p.rapidapi.com'
-	}
-}
-const apiUrl = `https://cnbc.p.rapidapi.com/symbols/translate?symbol=TSLA`;
-const newsContainer = document.getElementById("newsContainter");
+const apiKey = "c31c3ac3957d4c3ca612461aff466f74";
+const newsContainers = document.getElementById("newsContainer"); // Ensure this element exists in your HTML
 
-
-async function fetchNews(){
+async function fetchNews() {
 	try {
-		const response = await fetch(apiUrl,options);
+		const apiUrl = `https://newsapi.org/v2/top-headlines?sources=techcrunch&pageSize6&apiKey=${apiKey}`
+		const response = await fetch(apiUrl);
 		const result = await response.json();
-		return console.log(result);
+		 // Log the entire response for debugging
+		return result.articles;
 	} catch (error) {
 		console.error("Error, please try again!", error);
-		return[];
+		return [];
 	}
-	
 }
 
+function displayNews(articles) {
+		newsContainers.innerHTML = "";
+		articles.forEach((article) => {
+			const newsCard = document.createElement("div")
+			newsCard.classList.add("newsCard")
+			const img = document.createElement("img")
+			img.src = article.urlToImage;
+			// img.alt = article.title;
+			const title = document.createElement("h2")
+			title.textContent = article.title;
 
-function displayNews(articles){
-	newsContainer.innerHTML= "";
-	articles.forEach((article) => {
-		const newsCard = duocument.createElement("div");
-		const img = document.createElement("img");
-		img.src = article.urlToImage;
-		img.alt = article.title;
-		const title = document.createElement("h2");
-		const desc = document.createElement("p");
-		desc.textContent = article.desc;
+			newsCard.appendChild(img);
+			newsCard.appendChild(title);
+			newsCard.addEventListener("click", () =>{window.open(article.url, "_blank")});
+			newsContainers.appendChild(newsCard);
 
-		newsCard.appendChild(img);
-		newsCard.appendChild(title);
-		newsCard.appendChild(desc);
-		newsContainer.appendChild(newsCard);
-
-
-		
-	});
-
-
+			if (!newsContainers) {
+				console.error("newsContainer element not found.");
+				return;
+			}
+		});	
 }
 
 
 (async () => {
-	try{
+	try {
 		const articles = await fetchNews();
 		displayNews(articles);
-
-	}catch(error){
-		console.error("Error, please try again!",error);
-
+	} catch (error) {
+		console.error("Error, please try again!", error);
 	}
 })();
